cd9751b1b360d254b4170bbb623155ab93f1436f
Jackrim1 <jriminton@gmail.com>
2018-04-03 18:18:51 +0100

    up to wrong details in users

---
+++ app/views/applications/_application.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! application, :id, :user_id, :client_first_name, :cleint_last_name, :amount, :advisor_name, :client, :created_at, :updated_at
+json.url application_url(application, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403164915) do
+ActiveRecord::Schema.define(version: 20180403170626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "client_first_name"
+    t.string "client_last_name"
+    t.integer "amount"
+    t.string "advisor_name"
+    t.string "client_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +44,5 @@ ActiveRecord::Schema.define(version: 20180403164915) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applications", "users"
 end
